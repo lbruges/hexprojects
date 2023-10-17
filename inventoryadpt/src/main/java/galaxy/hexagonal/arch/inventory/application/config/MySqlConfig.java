2ba07888f9256ee567f452e3382fc521d108b69a
Laura B <laura.bruges.h@gmail.com>
2023-10-16 21:03:24 -0500

    Adding missing mysql beans

--- inventoryadpt/src/main/java/galaxy/hexagonal/arch/inventory/application/config/MySqlConfig.java
+++ inventoryadpt/src/main/java/galaxy/hexagonal/arch/inventory/application/config/MySqlConfig.java
@@ -3,17 +3,23 @@ package galaxy.hexagonal.arch.inventory.application.config;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.MySqlAdapterInventory;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.MySqlCatalogAdapter;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.MySqlFreezeAdapter;
+import galaxy.hexagonal.arch.inventory.adapter.out.mysql.MySqlRentalAdapter;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.mapper.ModelMapper;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.mapper.ProductMapper;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.mapper.CatalogItemMapper;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.mapper.RentableVehicleMapper;
+import galaxy.hexagonal.arch.inventory.adapter.out.mysql.mapper.RentedVehicleMapper;
+import galaxy.hexagonal.arch.inventory.adapter.out.mysql.mapper.RenterMapper;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.repository.FreezeRepository;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.repository.ModelRepository;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.repository.ProductRepository;
+import galaxy.hexagonal.arch.inventory.adapter.out.mysql.repository.RentalRepository;
+import galaxy.hexagonal.arch.inventory.adapter.out.mysql.repository.RenterRepository;
 import galaxy.hexagonal.arch.inventory.adapter.out.mysql.repository.VehicleRepository;
 import galaxy.hexagonal.arch.inventory.application.port.out.catalog.GenericCatalogPort;
 import galaxy.hexagonal.arch.inventory.application.port.out.freeze.GenericFreezePort;
 import galaxy.hexagonal.arch.inventory.application.port.out.inventory.GenericInventoryPort;
+import galaxy.hexagonal.arch.inventory.application.port.out.rental.GenericRentalPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -43,6 +49,16 @@ public class MySqlConfig {
     }
 
     @Bean
+    public RenterMapper renterMapper() {
+        return new RenterMapper();
+    }
+
+    @Bean
+    public RentedVehicleMapper rentedVehicleMapper() {
+        return new RentedVehicleMapper(renterMapper(), modelMapper());
+    }
+
+    @Bean
     public GenericCatalogPort genericCatalogPort(VehicleRepository vehicleRepository) {
         return new MySqlCatalogAdapter(vehicleRepository, vehicleMapper());
     }
@@ -57,6 +73,13 @@ public class MySqlConfig {
     @Bean
     public GenericFreezePort genericFreezePort(VehicleRepository vehicleRepository, FreezeRepository freezeRepository) {
         return new MySqlFreezeAdapter(vehicleRepository, freezeRepository, rentableVehicleMapper());
+    }
+
+    @Bean
+    public GenericRentalPort genericRentalPort(VehicleRepository vehicleRepository, RentalRepository rentalRepository,
+                                               RenterRepository renterRepository) {
+        return new MySqlRentalAdapter(vehicleRepository, rentalRepository, renterRepository, renterMapper(),
+                rentedVehicleMapper());
     }
 
 }
